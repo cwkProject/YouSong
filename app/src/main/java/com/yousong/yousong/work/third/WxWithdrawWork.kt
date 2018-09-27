33450bf9244ec9a33921561c9cb7228a4f017749
超悟空 <39251474@163.com>
2018-09-27 23:38:28 +0800

    修复大量bug

--- app/src/main/java/com/yousong/yousong/work/third/WxWithdrawWork.kt
+++ app/src/main/java/com/yousong/yousong/work/third/WxWithdrawWork.kt
@@ -16,7 +16,7 @@ import java.math.BigDecimal
 class WxWithdrawWork : BaseSimpleWorkModel<BigDecimal, Unit>() {
 
     override fun onFillParams(dataMap: MutableMap<String, String?>, vararg params: BigDecimal?) {
-        dataMap["amount"] = params[0]?.multiply(BigDecimal(100))?.toPlainString()
+        dataMap["amount"] = params[0]?.multiply(BigDecimal(100))?.stripTrailingZeros()?.toPlainString()
     }
 
     override fun onSuccessExtract(jsonResult: JSONObject) = Unit
